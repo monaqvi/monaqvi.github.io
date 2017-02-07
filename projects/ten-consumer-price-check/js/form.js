2c4f6d470607428b2b3412b32404c87f8d18fda1
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2017-02-06 16:13:55 -0800

    Add remove criteria button

--- projects/ten-consumer-price-check/js/form.js
+++ projects/ten-consumer-price-check/js/form.js
@@ -1,6 +1,11 @@
 $(function() {
+  var latestSet;
   $('#speak_with_add_criteria').click(function() {
-    var questions = $('#query-template > div').clone();
-    $('#speak_with').append(questions);
+    latestSet = $('#query-template > div').clone();
+    $('#speak_with').append(latestSet);
+  });
+
+  $('#speak_with_remove_criteria').click(function() {
+    if($('#speak_with > div').length > 1) $('#speak_with > div:last-of-type').remove();
   });
 });
