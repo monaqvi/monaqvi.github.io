457e07b7ef02b0b66789c046223209e0efb10082
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2017-02-08 13:49:37 -0800

    Remove tooltips when group completed

--- projects/ten-consumer-price-check/js/form.js
+++ projects/ten-consumer-price-check/js/form.js
@@ -3,15 +3,45 @@ $(function() {
     var latestSet;
     $('#speak_with_add_criteria').click(function() {
       latestSet = $('#query-template > div').clone();
-      $('#speak_with').append(latestSet);
+      $('#speak_with .queries').append(latestSet);
       $('#speak_with_remove_criteria').show()
     });
 
     $('#speak_with_remove_criteria').click(function() {
-      var numFields = $('#speak_with > div').length;
-      if(numFields > 1) $('#speak_with > div:last-of-type').remove();
+      var numFields = $('#speak_with > .queries > div').length;
+      if(numFields > 1) $('#speak_with .queries > div:last-of-type').remove();
       if(numFields === 2) $('#speak_with_remove_criteria').hide();
     });
+  })();
+
+  (function removeTooltips() {
+    var queryGroups = $('.query-group');
+    
+    var first = $(queryGroups[0]),
+        second = $(queryGroups[1]);
+
+    first.change(function() {
+      toggleTooltip.call(this);
+    });
+
+    first.keypress(function() {
+      toggleTooltip.call(this);
+    });
+
+    second.change(function() {
+      toggleTooltip.call(this);
+    });
+
+    second.keypress(function() {
+      toggleTooltip.call(this);
+    });
+
+    function toggleTooltip() {
+      var self = $(this);
+      var valid = !self.find('input:invalid, select:invalid, textarea:invalid').length;
+      var tooltip = $(self.find('.tooltip'));
+      valid ? tooltip.hide(50) : tooltip.show(50);
+    }
   })();
 
   $('#submit').click(function() {
@@ -25,7 +55,7 @@ $(function() {
       // Important to add to parent to not overwrite individual field ids
       $(invalid[0]).parent().attr('id', 'invalid');
     } else {
-      // var save = { source: window.location.pathname.slice(1), };
+      // var save = { source: window.location.pathname, };
 
       // $.ajax({
       //   method: 'POST',
@@ -33,7 +63,7 @@ $(function() {
       //   data: save,
       //   success: function(res) {
       //     console.log(res);
-      //     window.location.href = '/thankyou/';
+      //     window.location.href = '../thankyou/';
       //   }
       // });
     }
