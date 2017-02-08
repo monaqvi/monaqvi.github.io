74b21a9724835478dd3f02bc19e6ee30f75c3dd4
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2017-02-07 23:04:59 -0800

    Hide remove button when not applicable

--- projects/ten-consumer-price-check/js/form.js
+++ projects/ten-consumer-price-check/js/form.js
@@ -3,9 +3,12 @@ $(function() {
   $('#speak_with_add_criteria').click(function() {
     latestSet = $('#query-template > div').clone();
     $('#speak_with').append(latestSet);
+    $('#speak_with_remove_criteria').show()
   });
 
   $('#speak_with_remove_criteria').click(function() {
-    if($('#speak_with > div').length > 1) $('#speak_with > div:last-of-type').remove();
+    var numFields = $('#speak_with > div').length;
+    if(numFields > 1) $('#speak_with > div:last-of-type').remove();
+    if(numFields === 2) $('#speak_with_remove_criteria').hide();
   });
 });
